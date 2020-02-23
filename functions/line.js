020d8c7861d00e9ff04d1449a0e6aefd836da5c5
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-02-23 10:53:36 +0900

    LINEから送られた画像をICOSにアップロード

--- functions/line.js
+++ functions/line.js
@@ -1,16 +1,22 @@
 const lineBot = require("@line/bot-sdk");
+const requestPromise = require("request-promise");
+const aws = require('ibm-cos-sdk');
+const dateFns = require('date-fns');
+
+let botConfig;
+let botClient;
 
 exports.handler = function(context, event, callback) {
-  const botConfig = {
+  botConfig = {
     channelAccessToken: context.LINE_BOT_ACCESS_TOKEN,
     channelSecret: context.LINE_BOT_CHANNEL_SECRET
   }
-  const botClient = new lineBot.Client(botConfig);
+  botClient = new lineBot.Client(botConfig);
 
-  console.log(`bot called!!`);
+  // TODO: HeaderのValidation
 
   const  body = event;
-  console.log(`body=${JSON.stringify(body)}`);
+  // console.log(`body=${JSON.stringify(body)}`);
 
   if (body === null) {
     throw new Error('body parsing failed');
@@ -21,13 +27,68 @@ exports.handler = function(context, event, callback) {
     const msgEvtType = webhookData.type;
     const timeStamp = webhookData.timestamp;
     const userId = webhookData.source.userId;
+    const messageId = webhookData.message.id;
 
     switch (msgEvtType) {
-      case 'message': // メッセージ送信時
-      botClient.replyMessage(replyToken, {type: 'text', text: 'ありがとう'});
-      break;
-    }
+      case 'follow': // 友達追加時
+        // TODO: 使い方を説明するメッセージを送信
+
+      case 'message': // メッセージ受信時
+        const messageType = webhookData.message.type;
+        switch (messageType) {
+          case 'text': // テキストメッセージの場合
+            botClient.replyMessage(replyToken, {type: 'text', text: 'textありがとう'});
+            break;
+
+          case 'image': // 画像の場合
+            botClient.pushMessage(userId, {type: 'text', text: 'image読込中'});
+            const binImage = await getLineImage(messageId, replyToken);
+
+            const cos = new aws.S3({
+              endpoint: context.ICOS_ENDPOINT,
+              apiKeyId: context.ICOS_API_KEY,
+              ibmAuthEndpoint: 'https://iam.ng.bluemix.net/oidc/token',
+              serviceInstanceId: context.ICOS_RESOURCE_ID
+            });
+
+            const filename = dateFns.format(new Date(), 'yyyyMMddHHmmss') + '_grampus.jpeg';
+            const imageUrl = `https://${context.ICOS_BUCKET_NAME}.${context.ICOS_ENDPOINT}/${filename}`;
+            console.log(`url=${imageUrl}`);
+
+            if(binImage) {
+              await cos.putObject({
+                Bucket: context.ICOS_BUCKET_NAME,
+                Key: filename,
+                ContentType: 'image/jpeg',
+                ACL: 'public-read',
+                Body: binImage
+              }).promise();
+            }
+            break;
+        }
+      }
   });
 
   callback(null, {statusCode: 200});
 };
+
+async function getLineImage(messageId, replyToken) {
+  const options = {
+    url: `https://api.line.me/v2/bot/message/${messageId}/content`,
+    method: 'get',
+    headers: {
+      'Authorization': 'Bearer ' + botConfig.channelAccessToken,
+    },
+    encoding: null,
+  }
+  let binImage;
+  try{
+    binImage = await requestPromise(options);
+    await botClient.replyMessage(replyToken, {type: 'text', text: '画像取得に成功しました'});
+  } catch (err) {
+    console.log(`getLineImage: error=${err}`);
+    await botClient.replyMessage(replyToken, {type: 'text', text: '画像取得に失敗しました'});
+  }
+
+  return binImage;
+}
